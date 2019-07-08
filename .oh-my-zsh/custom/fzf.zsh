adb65a459c910c915cafb8e0729415c50b78ecc9
Steve Loveless <steve@agrian.com>
2019-07-08 15:07:10 -0700

    Remove commented stuff

--- .oh-my-zsh/custom/fzf.zsh
+++ .oh-my-zsh/custom/fzf.zsh
@@ -7,60 +7,8 @@
 # --hidden: Search hidden files and folders
 # --follow: Follow symlinks
 # --glob: Additional conditions for search (in this case ignore everything in the .git/ folder)
+
 # export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 # export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules,vendor/bundle,target,doc}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-# bind -x '"\C-p": vim $(fzf);'
-
-# fstash - easier way to deal with stashes
-# type fstash to get a list of your stashes
-# enter shows you the contents of the stash
-# ctrl-d shows a diff of the stash against your current HEAD
-# ctrl-b checks the stash out as a branch, for easier merging
-fstash() {
-  zmodload zsh/mapfile
-
-  local out q k sha
-  while out=$(
-    git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
-      fzf --ansi --no-sort --query="$q" --print-query \
-      --expect=ctrl-d,ctrl-b);
-  do
-    # mapfile -t out <<< "$out"
-    $mapfile[out] <<< "$out"
-    q="${out[0]}"
-    k="${out[1]}"
-    sha="${out[-1]}"
-    sha="${sha%%}"
-
-    [[ -z "$sha" ]] && continue
-    if [[ "$k" == 'ctrl-d' ]]; then
-      git diff $sha
-    elif [[ "$k" == 'ctrl-b' ]]; then
-      git stash branch "stash-$sha" $sha
-      break;
-    else
-      git stash show -p $sha
-    fi
-  done
-}
-
-unalias z 2> /dev/null
-z() {
-  if [[ -z "$*" ]]; then
-    cd "$(_z -l 2>&1 | fzf +s --tac | sed 's/^[0-9,.]* *//')"
-  else
-    _z "$@"
-  fi
-}
-
-
-# chruby integreation
-frb() {
-  local rb
-  rb=$((echo system; chruby | cut -c 4-) |
-    awk '{print $1}' |
-    fzf-tmux -l 30 +m --reverse)
-  chruby $rb
-}
