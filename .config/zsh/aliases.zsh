c096950d5431de9c737b15ac0566bc2858c8be5a
Steve Loveless <steve@agrian.com>
2019-07-08 17:20:20 -0700

    move zsh config stuff

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,7 @@
+alias la='ls -lA'
+alias ll='ls -lA'
+# alias ll="exa -la"
+alias el="exa -lah@ --git"
+
+alias zshconfig="nvim ~/.zshrc"
+alias ohmyzsh="nvim ~/.oh-my-zsh"
---
+++ .config/zsh/ansible.zsh
@@ -0,0 +1,1 @@
+ANSIBLE_HOSTS="~/.config/ansible/hosts"
