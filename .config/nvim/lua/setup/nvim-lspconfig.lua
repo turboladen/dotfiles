3802f7ac03ba2a3f4f1fc0f201308b34d53c7b59
Steve Loveless <steve.loveless@gmail.com>
2022-02-10 11:44:45 -0800

    nvim: try to fix fidget loading

--- .config/nvim/lua/setup/nvim-lspconfig.lua
+++ .config/nvim/lua/setup/nvim-lspconfig.lua
@@ -5,6 +5,7 @@ vim.cmd(
 augroup lsp_format_config
   autocmd!
   autocmd BufWritePost * lua vim.lsp.buf.formatting_sync()
+  autocmd BufReadPost * lua require("fidget").setup({})
 augroup end
 ]]
 )
