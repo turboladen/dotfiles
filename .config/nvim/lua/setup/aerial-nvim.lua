817c4e1503bfac6931ea951ff80699b0c5b0afb6
Steve Loveless <steve.loveless@gmail.com>
2022-11-04 10:04:51 -0700

    fix: nvim: Update aerial settings for deprecations

--- .config/nvim/lua/setup/aerial-nvim.lua
+++ .config/nvim/lua/setup/aerial-nvim.lua
@@ -1,31 +1,21 @@
 require("aerial").setup(
   {
-    default_direction = "prefer_left",
-    placement_editor_edge = true,
-    filter_kind = false
+    filter_kind = false,
+    attach_mode = "global",
+    on_attach = function(bufnr)
+      local opts = { noremap = true, silent = true, buffer = bufnr }
+
+      -- Aerial does not set any mappings by default, so you'll want to set some up
+      -- Toggle the aerial window with <leader>a
+      vim.keymap.set("n", "<leader>aa", "<cmd>AerialToggle!<CR>", opts)
+
+      -- Jump forwards/backwards with '{' and '}'
+      vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", opts)
+      vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", opts)
+
+      -- Jump up the tree with '[[' or ']]'
+      vim.keymap.set("n", "[[", "<cmd>lua require('aerial').prev_up()<CR>", opts)
+      vim.keymap.set("n", "]]", "<cmd>lua require('aerial').next_up()<CR>", opts)
+    end
   }
 )
-
-local aerial = {}
-
-function aerial.setup(client, bufnr)
-  require("aerial").on_attach(client, bufnr)
-  local opts = { noremap = true, silent = true }
-
-  -- Aerial does not set any mappings by default, so you'll want to set some up
-  -- Toggle the aerial window with <leader>a
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>aa", "<cmd>AerialToggle!<CR>", opts)
-
-  -- Jump forwards/backwards with '{' and '}'
-   vim.api.nvim_buf_set_keymap(bufnr, "n", "{", "<cmd>AerialPrev<CR>", opts)
-   vim.api.nvim_buf_set_keymap(bufnr, "n", "}", "<cmd>AerialNext<CR>", opts)
-
-  -- Jump up the tree with '[[' or ']]'
-   vim.api.nvim_buf_set_keymap(bufnr, "n", "[[", "<cmd>AerialPrevUp<CR>", opts)
-   vim.api.nvim_buf_set_keymap(bufnr, "n", "]]", "<cmd>AerialNextUp<CR>", opts)
-
-   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>at", "<cmd>ArealTreeToggle<CR>", opts)
-   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ao", "<cmd>ArealTreeOpenAll<CR>", opts)
-end
-
-return aerial
