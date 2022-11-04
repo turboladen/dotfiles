2c5cc9431c61d19250ef07174bff71e7766525ce
Steve Loveless <steve.loveless@gmail.com>
2022-11-04 10:06:39 -0700

    fix: nvim: Remove commented code in nvim-lspconfig

--- .config/nvim/lua/setup/nvim-lspconfig.lua
+++ .config/nvim/lua/setup/nvim-lspconfig.lua
@@ -1,10 +1,10 @@
 require("turboladen.lsp").setup_lsp()
 
+  -- autocmd BufWritePost * lua vim.lsp.buf.formatting_sync()
 vim.cmd(
   [[
 augroup lsp_format_config
   autocmd!
-  autocmd BufWritePost * lua vim.lsp.buf.formatting_sync()
   autocmd BufReadPost * lua require("fidget").setup({})
 augroup end
 ]]
