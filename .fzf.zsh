2dd1ded5138cc60f6fa5203d676848d9390dab17
Steve Loveless <steve.loveless@gmail.com>
2023-06-07 08:53:39 -0700

    fzf: Fix PATH and completion commands

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,12 +1,12 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == ${HOMEBREW_PREFIX}/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/${HOMEBREW_PREFIX}/opt/fzf/bin"
+  export PATH="${PATH:+${PATH}:}${HOMEBREW_PREFIX}/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "${HOMEBREW_PREFIX}/opt/fzf/shell/completion.zsh" 3> /dev/null
+[[ $- == *i* ]] && source "${HOMEBREW_PREFIX}/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
