8e6e2f2b7992fab4ae4ccb89926ef0d5ec21af49
Steve Loveless <steve.loveless@gmail.com>
2024-02-07 12:40:55 -0800

    nvim: Review options

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -150,9 +150,6 @@ return {
     "mvllow/modes.nvim",
     tag = "v0.2.1",
     event = "VeryLazy",
-    init = function()
-      vim.opt.cursorline = true
-    end,
     config = function()
       require("modes").setup()
     end
--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -1,59 +1,57 @@
--- ╭───────────────────────────────────────────────────╮
--- │ 0. Load special things before plugins stuff first │
--- ╰───────────────────────────────────────────────────╯
-vim.opt.termguicolors = true
+--          ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+--          ┃            Organized according to `:options`            ┃
+--          ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
 --  ╭──────────────────────────────────────────╮
 --  │ 2. moving around, searching and patterns │
 --  ╰──────────────────────────────────────────╯
--- vim.opt.smartcase = true
 
 --  ╭────────────────────╮
 --  │ 4. displaying text │
 --  ╰────────────────────╯
+vim.opt.scrolloff = 2
+vim.opt.linebreak = true
 vim.opt.breakindent = true
+vim.opt.sidescroll = 4
+vim.opt.sidescrolloff = 5
 vim.opt.cmdheight = 2
--- vim.opt.linebreak = true
+vim.opt.lazyredraw = true
 vim.opt.list = true
 vim.opt.listchars = "tab:▸ ,trail:.,nbsp:_,extends:❯,precedes:❮"
 vim.opt.number = true
-vim.opt.scrolloff = 2
-vim.opt.showtabline = 2
 
--- set guifont=MonoLisaCustom\ Nerd\ Font:h13:i
 vim.opt.guifont = 'MonoLisaCustom Nerd Font:h13:i'
 
 -- ╭──────────────────────────────────────╮
 -- │ 5. syntax, highlighting and spelling │
 -- ╰──────────────────────────────────────╯
+vim.opt.synmaxcol = 250
+vim.opt.termguicolors = true
+vim.opt.cursorcolumn = false -- this defaults to false, but is getting set somewhere
+-- vim.opt.cursorline = true
 vim.opt.colorcolumn = "80,120"
-vim.opt.cursorline = true
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
+
 vim.opt.spell = true
 vim.opt.spelllang = "en_us"
-vim.opt.synmaxcol = 250
-vim.opt.tabstop = 2
-vim.opt.termguicolors = true
 
 --  ╭─────────────────────╮
 --  │ 6. Multiple windows │
 --  ╰─────────────────────╯
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+vim.opt.splitkeep = "screen"
 
 --  ╭───────────────────────╮
 --  │ 7. multiple tab pages │
 --  ╰───────────────────────╯
+vim.opt.showtabline = 2
+
 --  ╭─────────────╮
 --  │ 8. terminal │
 --  ╰─────────────╯
 --  ╭─────────────────────╮
 --  │  9. using the mouse │
 --  ╰─────────────────────╯
---  10. messages and info
-
 --  ╭───────────────────────╮
 --  │ 10. Messages and info │
 --  ╰───────────────────────╯
@@ -62,44 +60,48 @@ vim.opt.shortmess:append('c')
 --  ╭────────────────────╮
 --  │ 11. Selecting text │
 --  ╰────────────────────╯
--- vim.opt.clipboard = "unnamed,unnamedplus"
-vim.opt.completeopt = "menu,menuone,noselect"
-vim.opt.undofile = true
 
 --  ╭──────────────────╮
 --  │ 12. Editing text │
 --  ╰──────────────────╯
+-- vim.opt.undofile = true
+vim.opt.completeopt = "menu,menuone,noselect"
+
 --  ╭────────────────────────╮
 --  │ 13. Tabs and indenting │
 --  ╰────────────────────────╯
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.smartindent = true
-vim.opt.softtabstop = 2
 vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
+vim.opt.smartindent = true
 
 --  ╭─────────────╮
 --  │ 14. Folding │
 --  ╰─────────────╯
 vim.opt.foldenable = false
 
+--  ╭───────────────╮
+--  │ 15. diff mode │
+--  ╰───────────────╯
+--  ╭───────────────╮
+--  │ 16. mapping   │
+--  ╰───────────────╯
 --  ╭───────────────────────────────╮
 --  │ 17. reading and writing files │
 --  ╰───────────────────────────────╯
 vim.opt.writebackup = false
+
 --  ╭───────────────────╮
 --  │ 18. the swap file │
 --  ╰───────────────────╯
 vim.opt.swapfile = false
-
 
 --  ╭──────────────────────────╮
 --  │ 19. command line editing │
 --  ╰──────────────────────────╯
 vim.opt.history = 300
 vim.opt.wildmode = { 'longest', 'full' }
-vim.opt.wildmenu = true
-
 vim.opt.wildignore = {
   -- Disable output and VCS files
   '*.o', '*.out', '*.obj', '*.rbc', '*.rbo',
@@ -116,7 +118,7 @@ vim.opt.wildignore = {
   -- Ignore simplecov generated coverage docs
   'coverage/*',
 }
-
+vim.opt.wildmenu = true
 
 --  ╭──────────────────────────────────╮
 --  │  20. executing external commands │
@@ -125,6 +127,7 @@ vim.opt.wildignore = {
 --  │  21. running make and jumping to errors (quickfix) │
 --  ╰────────────────────────────────────────────────────╯
 vim.opt.grepprg = "rg --vimgrep --files"
+
 --  ╭────────────────────────╮
 --  │  22. language specific │
 --  ╰────────────────────────╯
