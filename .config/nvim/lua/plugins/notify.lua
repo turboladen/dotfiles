cd00e31c2714ba84653f0b2adf17e6fc3a230e5c
Steve Loveless <steve.loveless@gmail.com>
2024-02-08 11:53:17 -0800

    nvim: Extract notify

---
+++ .config/nvim/lua/plugins/notify.lua
@@ -0,0 +1,46 @@
+--  ╭────────────────────────────────────────────────────────╮
+--  │ A fancy, configurable, notification manager for NeoVim │
+--  ╰────────────────────────────────────────────────────────╯
+local Plugin = { "rcarriga/nvim-notify" }
+
+Plugin.event = "VeryLazy"
+
+Plugin.dependencies = {
+  "nvim-telescope/telescope.nvim",
+}
+
+Plugin.init = function()
+  vim.opt.termguicolors = true
+end
+
+Plugin.opts = {
+  render = "wrapped-compact"
+}
+
+Plugin.config = function(opts)
+  local notify = require("notify")
+  vim.notify = notify
+  notify.setup(opts)
+
+  local telescope = require("telescope")
+  telescope.load_extension("notify")
+
+  vim.lsp.handlers['window/showMessage'] = function(_, result, ctx)
+    local client = vim.lsp.get_client_by_id(ctx.client_id)
+    local lvl = ({
+      'ERROR',
+      'WARN',
+      'INFO',
+      'DEBUG',
+    })[result.type]
+    notify({ result.message }, lvl, {
+      title = 'LSP | ' .. client.name,
+      timeout = 10000,
+      keep = function()
+        return lvl == 'ERROR' or lvl == 'WARN'
+      end,
+    })
+  end
+end
+
+return Plugin
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -59,48 +59,6 @@ return {
   --   }
   -- },
 
-  --  ╭────────────────────────────────────────────────────────╮
-  --  │ A fancy, configurable, notification manager for NeoVim │
-  --  ╰────────────────────────────────────────────────────────╯
-  {
-    "rcarriga/nvim-notify",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-    },
-    init = function()
-      vim.opt.termguicolors = true
-    end,
-    opts = {
-      render = "wrapped-compact"
-    },
-    config = function(opts)
-      local notify = require("notify")
-      vim.notify = notify
-      notify.setup(opts)
-
-      local telescope = require("telescope")
-      telescope.load_extension("notify")
-
-      vim.lsp.handlers['window/showMessage'] = function(_, result, ctx)
-        local client = vim.lsp.get_client_by_id(ctx.client_id)
-        local lvl = ({
-          'ERROR',
-          'WARN',
-          'INFO',
-          'DEBUG',
-        })[result.type]
-        notify({ result.message }, lvl, {
-          title = 'LSP | ' .. client.name,
-          timeout = 10000,
-          keep = function()
-            return lvl == 'ERROR' or lvl == 'WARN'
-          end,
-        })
-      end
-    end
-  },
-
   {
     "j-hui/fidget.nvim",
     tag = "v1.0.0",
