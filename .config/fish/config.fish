233a29ff648f59c08938e64ebdaf48f865efbd3a
Steve Loveless <steve.loveless@gmail.com>
2024-12-16 08:58:57 -0800

    fish: Only show fish_greeting if is-interactive

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,16 +3,20 @@ set --export --prepend PATH "/Users/steve.loveless/.rd/bin"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
 
 if status is-login
-  # ╭────────────────────────╮
-  # │ Turn off fish greeting │
-  # ╰────────────────────────╯
-  set fish_greeting
   set -gx EDITOR /opt/homebrew/bin/nvim
   set -gx HOMEBREW_CASK_OPTS "--appdir=~/Applications"
 
   # https://github.com/sharkdp/bat?tab=readme-ov-file#man
   # set -gx MANPAGER="sh -c 'col -bx | bat -l man -p'"
   set -gx MANPAGER sh\ -c\ \'col\ -bx\ \|\ bat\ -l\ man\ -p\'
+
+end
+
+if status is-interactive
+  # ╭────────────────────────╮
+  # │ Turn off fish greeting │
+  # ╰────────────────────────╯
+  set fish_greeting
 
   # ╭─────────╮
   # │ Aliases │
