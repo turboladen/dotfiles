fc30902ed9316812eeb21386cbe5e22171b36c8e
Steve Loveless <steve.loveless@gmail.com>
2024-12-16 09:22:54 -0800

    nvim: telescope_fzf_native: Ditch fuzzy matching
    Trying out for now; it's a bit annoying, but gonna roll with it for a
    bit.

--- .config/nvim/lua/plugins/telescope_fzf_native_nvim.lua
+++ .config/nvim/lua/plugins/telescope_fzf_native_nvim.lua
@@ -1,15 +1,14 @@
 local Plugin = { 'nvim-telescope/telescope-fzf-native.nvim' }
 
-Plugin.build =
-'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+Plugin.build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
 
 Plugin.extension_config = function()
   return {
-    fuzzy = true,                       -- false will only do exact matching
-    override_generic_sorter = true,     -- override the generic sorter
-    override_file_sorter = true,        -- override the file sorter
-    case_mode = "smart_case",           -- or "ignore_case" or "respect_case"
-    -- the default case_mode is "smart_case"
+    -- fuzzy = true,                   -- false will only do exact matching
+    -- override_generic_sorter = true, -- override the generic sorter
+    -- override_file_sorter = true,    -- override the file sorter
+    -- case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+    case_mode = "respect_case", -- or "ignore_case" or "respect_case"
   }
 end
 
