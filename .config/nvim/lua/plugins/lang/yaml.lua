ac3326722479adb3c6595ae57e92542c84412ca1
Steve Loveless <steve.loveless@gmail.com>
2025-06-30 00:25:35 -0700

    nvim.lang.yaml: add file; add ftplugin

---
+++ .config/nvim/lua/plugins/lang/yaml.lua
@@ -0,0 +1,80 @@
+return {
+  -- Treesitter YAML parser
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "yaml" },
+    },
+  },
+
+  -- Schema store for YAML validation
+  {
+    "b0o/SchemaStore.nvim",
+    lazy = true,
+    version = false, -- last release is way too old
+  },
+
+  -- YAML LSP configuration
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "b0o/SchemaStore.nvim",
+    },
+    opts = function()
+      -- Configure yamlls server
+      vim.lsp.config("yamlls", {
+        cmd = { "yaml-language-server", "--stdio" },
+        filetypes = { "yaml", "yml" },
+        root_markers = {
+          ".git",
+          "docker-compose.yml",
+          "docker-compose.yaml",
+          ".github",
+          "helm",
+          "Chart.yaml",
+        },
+        -- Have to add this for yamlls to understand that we support line folding
+        capabilities = {
+          textDocument = {
+            foldingRange = {
+              dynamicRegistration = false,
+              lineFoldingOnly = true,
+            },
+          },
+        },
+        settings = {
+          redhat = { telemetry = { enabled = false } },
+          yaml = {
+            keyOrdering = false,
+            format = {
+              enable = true,
+            },
+            validate = true,
+            schemaStore = {
+              -- Must disable built-in schemaStore support to use
+              -- schemas from SchemaStore.nvim plugin
+              enable = false,
+              -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+              url = "",
+            },
+            schemas = require("schemastore").yaml.schemas(),
+          },
+        },
+      })
+
+      vim.lsp.enable("yamlls")
+    end,
+  },
+
+  -- YAML linting
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      -- Add YAML linters to existing configuration
+      opts.linters_by_ft = opts.linters_by_ft or {}
+      opts.linters_by_ft.yaml = { "yamllint" }
+      opts.linters_by_ft.yml = { "yamllint" }
+      return opts
+    end,
+  },
+}
