6d3ecffb3f8449424322a3d3f4835ae098d49c82
Steve Loveless <steve.loveless@gmail.com>
2025-06-30 17:35:53 -0700

    nvim.lang: add json

---
+++ .config/nvim/lua/plugins/lang/json.lua
@@ -0,0 +1,48 @@
+return {
+  -- Treesitter JSON parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "json", "json5", "jsonc" },
+    },
+  },
+
+  -- Schema store for JSON validation
+  {
+    "b0o/SchemaStore.nvim",
+    lazy = true,
+    version = false, -- last release is way too old
+  },
+
+  -- JSON LSP configuration
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "b0o/SchemaStore.nvim",
+    },
+    opts = function()
+      -- Configure jsonls server
+      vim.lsp.config("jsonls", {
+        cmd = { "vscode-json-language-server", "--stdio" },
+        filetypes = { "json", "jsonc" },
+        root_markers = {
+          "package.json",
+          "tsconfig.json",
+          ".git",
+        },
+        init_options = {
+          provideFormatter = true,
+        },
+        settings = {
+          json = {
+            schemas = require("schemastore").json.schemas(),
+            validate = { enable = true },
+            format = { enable = true },
+          },
+        },
+      })
+
+      vim.lsp.enable("jsonls")
+    end,
+  },
+}
