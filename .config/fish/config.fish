1ac8eb1380ca13c567a0d0f4d256ab138e3d7443
Steve Loveless <steve.loveless@gmail.com>
2025-07-01 23:31:04 -0700

    fish: cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,42 +2,37 @@
 set --export --prepend PATH "/Users/steve.loveless/.rd/bin"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
 
+# ╭─────────────────────╮
+# │ Login Configuration │
+# ╰─────────────────────╯
 if status is-login
-  set -gx EDITOR /opt/homebrew/bin/nvim
-  set -gx HOMEBREW_CASK_OPTS "--appdir=~/Applications"
+    set -gx EDITOR /opt/homebrew/bin/nvim
+    set -gx HOMEBREW_CASK_OPTS "--appdir=~/Applications"
 
-  # https://github.com/sharkdp/bat?tab=readme-ov-file#man
-  # set -gx MANPAGER="sh -c 'col -bx | bat -l man -p'"
-  set -gx MANPAGER sh\ -c\ \'col\ -bx\ \|\ bat\ -l\ man\ -p\'
+    # Use bat as manpager for syntax highlighting
+    set -gx MANPAGER sh\ -c\ \'col\ -bx\ \|\ bat\ -l\ man\ -p\'
 
-  # FZF integration with bat for file previews
-  set -gx FZF_DEFAULT_OPTS "--preview 'bat --style=numbers --color=always {}'"
+    # FZF integration with bat for file previews
+    set -gx FZF_DEFAULT_OPTS "--preview 'bat --style=numbers --color=always {}'"
 
+    # Add Postgres.app to PATH
+    fish_add_path ~/Applications/Postgres.app/Contents/Versions/latest/bin
+    fish_add_path ~/Applications/Postgres.app/Contents/Versions/latest/lib
 end
 
+# ╭───────────────────────────╮
+# │ Interactive Configuration │
+# ╰───────────────────────────╯
 if status is-interactive
-  # ╭────────────────────────╮
-  # │ Turn off fish greeting │
-  # ╰────────────────────────╯
-  set fish_greeting
+    # Disable fish greeting
+    set fish_greeting
 
-  # ╭─────────╮
-  # │ Aliases │
-  # ╰─────────╯
-  alias .. 'cd ..'
+    # Set up aliases
+    alias .. 'cd ..'
 
-  fish_vi_key_bindings
+    # Use vi key bindings
+    fish_vi_key_bindings
 
-  fastfetch
+    # Show system info on startup
+    fastfetch
 end
-
-# pnpm
-set -gx PNPM_HOME "/Users/steve.loveless/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
-# Postgres.app
-fish_add_path ~/Applications/Postgres.app/Contents/Versions/latest/bin
-fish_add_path ~/Applications/Postgres.app/Contents/Versions/latest/lib
