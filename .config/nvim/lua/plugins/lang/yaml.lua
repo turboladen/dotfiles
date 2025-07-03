85e22dd05f02ec432c89819961f2a6183fe768ad
Steve Loveless <steve.loveless@gmail.com>
2025-07-03 13:55:32 -0700

    nvim.yaml: update to use dprint for formatting

--- .config/nvim/lua/plugins/lang/yaml.lua
+++ .config/nvim/lua/plugins/lang/yaml.lua
@@ -3,75 +3,69 @@
 -- yamllint: pip install yamllint or brew install yamllint
 
 return {
-  -- Treesitter YAML parser
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = { "yaml" },
-    },
-  },
+	-- Treesitter YAML parser
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = { "yaml" },
+		},
+	},
 
-  -- Schema store for YAML validation
-  {
-    "b0o/SchemaStore.nvim",
-    lazy = true,
-    version = false, -- last release is way too old
-  },
+	-- Schema store for YAML validation
+	{
+		"b0o/SchemaStore.nvim",
+		lazy = true,
+		version = false, -- last release is way too old
+	},
 
-  -- YAML LSP configuration
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "b0o/SchemaStore.nvim",
-    },
-    opts = function()
-      -- Configure yamlls with SchemaStore integration
-      vim.lsp.config("yamlls", {
-        settings = {
-          yaml = {
-            schemaStore = {
-              -- Must disable built-in schemaStore support to use
-              -- schemas from SchemaStore.nvim plugin
-              enable = false,
-              -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-              url = "",
-            },
-            schemas = require("schemastore").yaml.schemas(),
-          },
-        },
-      })
+	-- YAML LSP configuration
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"b0o/SchemaStore.nvim",
+		},
+		opts = function()
+			-- Configure yamlls with SchemaStore integration
+			vim.lsp.config("yamlls", {
+				settings = {
+					yaml = {
+						schemaStore = {
+							-- Must disable built-in schemaStore support to use
+							-- schemas from SchemaStore.nvim plugin
+							enable = false,
+							-- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+							url = "",
+						},
+						schemas = require("schemastore").yaml.schemas(),
+					},
+				},
+			})
 
-      vim.lsp.enable("yamlls")
-    end,
-  },
+			vim.lsp.enable("yamlls")
+		end,
+	},
 
-  -- YAML formatting via conform.nvim
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        yaml = { "yamlfmt" },
-        yml = { "yamlfmt" },
-      },
-      formatters = {
-        yamlfmt = {
-          -- Uses global config at ~/.config/yamlfmt/.yamlfmt.yml
-          prepend_args = { "-formatter", "retain_line_breaks=true" },
-        },
-      },
-    },
-  },
+	-- YAML formatting via conform.nvim
+	{
+		"stevearc/conform.nvim",
+		optional = true,
+		opts = {
+			formatters_by_ft = {
+				yaml = { "dprint" },
+				yml = { "dprint" },
+			},
+		},
+	},
 
-  -- YAML linting
-  {
-    "mfussenegger/nvim-lint",
-    opts = function(_, opts)
-      -- Add YAML linters to existing configuration
-      opts.linters_by_ft = opts.linters_by_ft or {}
-      opts.linters_by_ft.yaml = { "yamllint" }
-      opts.linters_by_ft.yml = { "yamllint" }
-      return opts
-    end,
-  },
+	-- YAML linting
+	{
+		"mfussenegger/nvim-lint",
+		opts = function(_, opts)
+			-- Add YAML linters to existing configuration
+			opts.linters_by_ft = opts.linters_by_ft or {}
+			opts.linters_by_ft.yaml = { "yamllint" }
+			opts.linters_by_ft.yml = { "yamllint" }
+			return opts
+		end,
+	},
 }
