f152f98ac6fc6524bf289c6c7dcc0c9bbe8052d8
Steve Loveless <steve.loveless@gmail.com>
2026-04-08 13:42:36 -0700

    fix(treesitter) updates for 0.12.0

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,21 @@
 return {
-  -- Treesitter for better syntax highlighting and parsing
+  -- Treesitter: parser management only (new API for Neovim 0.12+)
+  -- highlight, indent, textobjects are no longer configured here
   {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "andymass/vim-matchup",
-    },
-    version = false, -- last release is way too old
+    version = false,
     build = ":TSUpdate",
     event = { "VeryLazy" },
-    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+    lazy = vim.fn.argc(-1) == 0,
     init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-      -- Luckily, the only things that those plugins need are the custom queries, which we make
-      -- available during startup.
+      -- Add nvim-treesitter queries to rtp early so other plugins (matchup, etc.)
+      -- can find them before the plugin fully loads
       require("lazy.core.loader").add_to_rtp(plugin)
       require("nvim-treesitter.query_predicates")
     end,
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    keys = {
-      { "<c-space>", desc = "Increment Selection" },
-      { "<bs>", desc = "Decrement Selection", mode = "x" },
-    },
     opts_extend = { "ensure_installed" },
-    ---@type TSConfig
     opts = {
-      highlight = { enable = true },
-      indent = { enable = true },
-      matchup = {
-        enable = true, -- mandatory, false will disable the whole extension
-        disable = {}, -- optional, list of language that will be disabled
-      },
       ensure_installed = {
         "diff",
         "html",
@@ -43,100 +26,8 @@ return {
         "vimdoc",
         "xml",
       },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-            ["ab"] = "@block.outer",
-            ["ib"] = "@block.inner",
-            ["al"] = "@loop.outer",
-            ["il"] = "@loop.inner",
-            ["ai"] = "@conditional.outer",
-            ["ii"] = "@conditional.inner",
-            ["as"] = "@statement.outer",
-            ["is"] = "@scopename.inner",
-            ["ak"] = "@comment.outer",
-          },
-          -- You can choose the select mode (default is charwise 'v')
-          selection_modes = {
-            ["@parameter.outer"] = "v", -- charwise
-            ["@function.outer"] = "V", -- linewise
-            ["@class.outer"] = "<c-v>", -- blockwise
-          },
-          -- If you set this to `true` (default is `false`) then any textobject is
-          -- extended to include preceding or succeeding whitespace. Succeeding
-          -- whitespace has priority in order to act similarly to eg the built-in `ap`.
-          include_surrounding_whitespace = true,
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]]"] = { query = "@class.outer", desc = "Next class start" },
-            ["]o"] = "@loop.*",
-            ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-          },
-          goto_next_end = {
-            ["]M"] = "@function.outer",
-            ["]["] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[m"] = "@function.outer",
-            ["[["] = "@class.outer",
-          },
-          goto_previous_end = {
-            ["[M"] = "@function.outer",
-            ["[]"] = "@class.outer",
-          },
-          goto_next = {
-            ["]d"] = "@conditional.outer",
-          },
-          goto_previous = {
-            ["[d"] = "@conditional.outer",
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["<leader>a"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["<leader>A"] = "@parameter.inner",
-          },
-        },
-        lsp_interop = {
-          enable = true,
-          border = "none",
-          floating_preview_opts = {},
-          peek_definition_code = {
-            ["<leader>df"] = "@function.outer",
-            ["<leader>dF"] = "@class.outer",
-          },
-        },
-      },
     },
-    ---@param opts TSConfig
     config = function(_, opts)
-      -- Simple deduplication of ensure_installed
       if type(opts.ensure_installed) == "table" then
         local seen = {}
         local deduped = {}
@@ -149,6 +40,36 @@ return {
         opts.ensure_installed = deduped
       end
       require("nvim-treesitter").setup(opts)
+      -- Enable treesitter highlighting and indent per buffer
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(ev)
+          if pcall(vim.treesitter.start, ev.buf) then
+            vim.bo[ev.buf].indentexpr = 'v:lua.require("nvim-treesitter").indentexpr()'
+          end
+        end,
+      })
+    end,
+  },
+
+  -- Textobject selection: replaces nvim-treesitter-textobjects (frozen/incompatible with new API)
+  -- Keymaps: af/if=function, ac/ic=class, aa/ia=parameter, al/il=loop, ai/ii=conditional, ak=comment
+  -- NOTE: move mappings (]m, [m, etc.) are not replicated here
+  {
+    "echasnovski/mini.ai",
+    event = "VeryLazy",
+    config = function()
+      local ai = require("mini.ai")
+      ai.setup({
+        n_lines = 500,
+        custom_textobjects = {
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
+          l = ai.gen_spec.treesitter({ a = "@loop.outer", i = "@loop.inner" }),
+          i = ai.gen_spec.treesitter({ a = "@conditional.outer", i = "@conditional.inner" }),
+          k = ai.gen_spec.treesitter({ a = "@comment.outer", i = "@comment.outer" }),
+          a = ai.gen_spec.treesitter({ a = "@parameter.outer", i = "@parameter.inner" }),
+        },
+      })
     end,
   },
 
@@ -157,12 +78,9 @@ return {
     "andymass/vim-matchup",
     event = { "BufReadPost" },
     config = function()
-      -- Enable treesitter integration
       vim.g.matchup_matchparen_offscreen = { method = "popup" }
       vim.g.matchup_matchparen_deferred = 1
       vim.g.matchup_matchparen_hi_surround_always = 1
-
-      -- Disable matchit (we use matchup instead)
       vim.g.loaded_matchit = 1
     end,
   },
